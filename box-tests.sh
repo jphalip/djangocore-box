da5811b24d950db6ccacce4412b5043aada5aece
Julien Phalip <jphalip@gmail.com>
2013-04-14 20:05:16 -0700

    Added some smoke tests for the VM build.

---
+++ box-tests.sh
@@ -0,0 +1,10 @@
+runtests2.6-spatialite gis
+runtests2.6-postgis gis
+runtests2.6-sqlite auth
+runtests2.7-sqlite auth
+runtests2.6-postgresql auth
+runtests2.6-mysql auth
+runtests3.3-sqlite auth
+runtests3.3-postgresql auth
+runtests2.6-sqlite admin_inlines.SeleniumFirefoxTests --selenium
+runtests2.6-sqlite
